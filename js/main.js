05635d6a91e7bb0602f7c2bd76b2cdb716ed4ef4
Tatjana14 <tanycha1403@gmail.com>
2021-05-13 21:13:36 +0300

    feedback

--- js/main.js
+++ js/main.js
@@ -21,3 +21,23 @@ $(document).ready(function() {
 		
 	});
 });
+
+
+new Swiper('.swiper-container', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    keyboard : {
+        enabled: true, 
+        onlyInViewport: true,
+        pageUpDown: true
+    },
+    autoHeight: true,
+    /* autoplay: {
+        delay: 2000,
+        stopOnLastSlide: false,
+        disableOnIteraction: false
+    }, */
+    speed: 500
+});
